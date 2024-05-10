e4006b28f5287e276182d01020891ecfdceb2d5a
Shekhar <shekharw2298@gmail.com>
2024-05-10 18:12:45 +0530

    Day7 project added

---
+++ Day7/ArrayMatrix/functionOverload.java
@@ -0,0 +1,10 @@
+package ArrayMatrix;
+
+public class functionOverload {
+    public void area(double radius){
+        System.out.println(radius*radius*Math.PI);
+    }
+    public static void triangle(int base, int height){
+        System.out.println(0.5*base*height);
+    }
+}
